393181d85291021225572bfa8ea4182e1a082252
Nir Magnezi <nmagnezi@redhat.com>
2014-12-18 09:32:46 +0200

    Fixes answer file path for jenkins

--- config/constants.py
+++ config/constants.py
@@ -8,7 +8,8 @@ REBOOT_SLEEP = 10
 
 # Paths
 INSTALLER_CONFIG_FILE_DEFAULT_PATH = '/root'
-INSTALLER_CONFIG_FILE_DIRCTORY = 'installer/packstack/configs'
+INSTALLER_CONFIG_FILE_DIRCTORY = 'hurricane/installer/packstack/configs'
+#INSTALLER_CONFIG_FILE_DIRCTORY = 'installer/packstack/configs'
 CONFIG_FILE_NAME = 'config.ini'
 CONFIG_FILE_DIRECTORY = 'hurricane_config/config'
 #CONFIG_FILE_DIRECTORY = 'config'
